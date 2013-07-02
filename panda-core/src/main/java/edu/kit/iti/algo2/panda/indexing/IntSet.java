ca2ca242a2b6fedd6f22869bfcad65c3f4852458
fermeise <ocker.christian@gmail.com>
2013-07-02 18:35:03 +0200

    Optimized k-gram indexing

---
+++ panda-core/src/main/java/edu/kit/iti/algo2/panda/indexing/IntSet.java
@@ -0,0 +1,52 @@
+package edu.kit.iti.algo2.panda.indexing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A fast set of integer values. The values have to be added in ascending order.
+ * It is checked that the same value is not added twice.
+ */
+public class IntSet {
+	private static final int initialSpace = 16;
+	
+	private int[] elements;
+	private int elementCount;
+	
+	protected IntSet() {
+		this.elements = new int[initialSpace];
+		this.elementCount = 0;
+	}
+	
+	protected int[] getElements() {
+		return elements;
+	}
+	
+	public int getElementCount() {
+		return elementCount;
+	}
+
+	protected void add(int id) {
+		if(elementCount > 0 && elements[elementCount - 1] == id) {
+			return;
+		}
+		if(elements.length == elementCount) {
+			int[] newElements = new int[elements.length * 2];
+			for(int i = 0; i < elements.length; i++) {
+				newElements[i] = elements[i];
+			}
+			elements = newElements;
+		}
+		elements[elementCount] = id;
+		elementCount++;
+	}
+	
+	public List<Integer> asList() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i < elementCount; i++) {
+			list.add(elements[i]);
+		}
+		
+		return list;
+	}
+}
--- panda-core/src/main/java/edu/kit/iti/algo2/panda/indexing/KGramIndex.java
+++ panda-core/src/main/java/edu/kit/iti/algo2/panda/indexing/KGramIndex.java
@@ -1,20 +1,17 @@
 package edu.kit.iti.algo2.panda.indexing;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
  * Simple index on splits of length k.
  */
 public class KGramIndex {
-	private final ArrayList<String> words = new ArrayList<>();
-	private final Map<String, List<Integer>> index = new HashMap<>();
+	private final ArrayList<String> words;
+	private final HashMap<String, IntSet> index;
 	private final int k;
 	
 	/**
@@ -23,6 +20,8 @@ public class KGramIndex {
 	 * @param k size of one token
 	 */
 	public KGramIndex(int k) {
+		this.words = new ArrayList<String>();
+		this.index = new HashMap<String, IntSet>();
 		this.k = k;
 	}
 	
@@ -46,12 +45,20 @@ public class KGramIndex {
 	 * @param word
 	 */
 	public void addWord(String word) {
-		int index = this.words.size();
-		List<String> kgrams = createFromWord(word);
-		if (!kgrams.isEmpty()) {
-			addToIndex(kgrams, index);
-			this.words.add(word);			
+		if(word.length() < k)
+			return;
+		
+		int wordIdx = words.size();
+		for(int i = 0; i <= word.length() - k; i++) {
+			String kgram = word.substring(i, i + k);
+			IntSet wordList = index.get(kgram);
+			if (wordList == null) {
+				wordList = new IntSet();
+				index.put(kgram, wordList);
+			}
+			wordList.add(wordIdx);
 		}
+		words.add(word);
 	}
 	
 	/**
@@ -66,34 +73,5 @@ public class KGramIndex {
 	 */
 	public Set<String> getKeys() {
 		return this.index.keySet();
-	}
-	
-	private void addToIndex(Iterable<String> kgrams, int wordIndex) {
-		for (String kgram : kgrams) {
-			getWordList(kgram).add(wordIndex);
-		}
-	}
-	
-	private List<Integer> getWordList(String kgram) {
-		List<Integer> result = index.get(kgram);
-		if (result == null) {
-			result = new LinkedList<>();
-			index.put(kgram, result);
-		}
-		return result;
-	}
-	
-	private List<String> createFromWord(String word) {
-		int initialSize = word.length() - k;
-		if (initialSize < 0)
-			return new ArrayList<>();
-		
-		List<String> result = new ArrayList<>(initialSize);
-		char[] chars = word.toCharArray();
-		for (int i=0; i <= chars.length - k; i++) {
-			char[] token = Arrays.copyOfRange(chars, i, i+k);
-			result.add(new String(token));
-		}
-		return result;
 	}
 }
