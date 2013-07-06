7a20b7f33897b90a675de81129b742acbe0c9e4c
alexander <ahantelmann@gmail.com>
2013-07-06 20:25:28 +0200

    * Added dummy code for status bar and menu bar * Changed code to use system look & feel

--- panda-gui/src/main/java/edu/kit/iti/algo2/panda/gui/Starter.java
+++ panda-gui/src/main/java/edu/kit/iti/algo2/panda/gui/Starter.java
@@ -4,8 +4,10 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Logger;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 import edu.kit.iti.algo2.panda.gui.model.AsyncQueryModel;
 import edu.kit.iti.algo2.panda.gui.model.QueryModel;
@@ -13,11 +15,17 @@ import edu.kit.iti.algo2.panda.gui.view.SearchView;
 import edu.kit.iti.algo2.panda.management.IndexManager;
 
 public class Starter implements Runnable {
+	private static final Logger LOG = Logger.getLogger("GUI");
 	
 	private final QueryModel model;
 	
 	public static void main(String[] args) throws IOException {
 		Path rootDirectory = Paths.get("../");
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			LOG.info("Cannot change Look & Feel.");
+		}
 		SwingUtilities.invokeLater(new Starter(rootDirectory));
 	}
 
--- panda-gui/src/main/java/edu/kit/iti/algo2/panda/gui/view/SearchView.java
+++ panda-gui/src/main/java/edu/kit/iti/algo2/panda/gui/view/SearchView.java
@@ -1,11 +1,18 @@
 package edu.kit.iti.algo2.panda.gui.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
@@ -18,12 +25,21 @@ public class SearchView {
 	private final JFrame root = new JFrame("PANDA Search");
 	
 	{
-		root.setLayout(new BorderLayout(10, 10));
-		root.getRootPane().setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		root.setLayout(new BorderLayout());
 		root.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 	
-	private final JPanel centerPanel = new JPanel(new BorderLayout(10, 10));
+	private final JMenuBar topMenu = new JMenuBar();
+	{
+		JMenu fileMenu = new JMenu("File");
+		JMenu settingsMenu = new JMenu("Settings");
+		fileMenu.add(new JMenuItem("Close"));
+		topMenu.add(fileMenu);
+		topMenu.add(settingsMenu);
+		root.setJMenuBar(topMenu);
+	}
+	
+	private final JPanel centerPanel = new JPanel(new BorderLayout());
 	private final JTextField searchField = new JTextField(30);
 	private final QueryModel model;
 	private final JList<String> searchResult = new JList<>();
@@ -32,9 +48,19 @@ public class SearchView {
 		searchResult.setPreferredSize(new Dimension(100, 300));
 		centerPanel.add(searchField, BorderLayout.NORTH);
 		searchField.getDocument().addDocumentListener(new QueryDocumentListener());
+		searchField.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 14));
 		searchResult.setBorder(BorderFactory.createTitledBorder("Result:"));
 		centerPanel.add(searchResult, BorderLayout.CENTER);
 		root.add(centerPanel);
+	}
+	
+	private final JPanel statusPanel = new JPanel();
+	{
+		statusPanel.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.gray));
+		statusPanel.setLayout(new BoxLayout(statusPanel, BoxLayout.X_AXIS));
+		JLabel label = new JLabel("Index finished");
+		statusPanel.add(label);
+		root.add(statusPanel, BorderLayout.SOUTH);
 	}
 	
 	public SearchView(QueryModel model) {
