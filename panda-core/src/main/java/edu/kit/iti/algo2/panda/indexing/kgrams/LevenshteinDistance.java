3bc12d1717b62d295d38b8023aeb831bd3a55523
alexander <ahantelmann@gmail.com>
2013-07-15 13:10:01 +0200

    Comments for Levenshtein functions.

--- panda-core/src/main/java/edu/kit/iti/algo2/panda/indexing/kgrams/LevenshteinDistance.java
+++ panda-core/src/main/java/edu/kit/iti/algo2/panda/indexing/kgrams/LevenshteinDistance.java
@@ -2,70 +2,92 @@ package edu.kit.iti.algo2.panda.indexing.kgrams;
 
 import java.util.Arrays;
 
+/**
+ * Container for functions to operate on Levenshtein distance.
+ */
 public class LevenshteinDistance {
-	public static int distance(char[] s, char[] t) {
-		if (Arrays.equals(s, t)) return 0;
-		if (s.length == 0) return t.length;
-		if (t.length == 0) return s.length;
+	
+	/**
+	 * Computes the edit distance between <code>first</code> and <code>second</code>
+	 * using weight 1 for replace, add and remove.
+	 * 
+	 * <p>Running time: O(|first| * |second|)</p>
+	 */
+	public static int distance(char[] first, char[] second) {
+		if (Arrays.equals(first, second)) return 0;
+		if (first.length == 0) return second.length;
+		if (second.length == 0) return first.length;
 		
-		int[] v0 = new int[t.length + 1];
-		int[] v1 = new int[t.length + 1];
+		int[] prevRow = new int[second.length + 1];
+		int[] currentRow = new int[second.length + 1];
 		
-		for (int i=0; i < v0.length; i++) {
-			v0[i] = i;
+		// Fill the first row with edit distance of replacing all letters
+		for (int row=0; row < prevRow.length; row++) {
+			prevRow[row] = row;
 		}
 		
-		for (int i=0; i < s.length; i++) {
-			v1[0] = i + 1;
+		// Compute the other rows until all letters of first are used.
+		for (int row=0; row < first.length; row++) {
+			currentRow[0] = row + 1;
 			
-			for (int j=0; j < t.length; j++) {
-				if (s[i] == t[j]) {
-					v1[j+1] = v0[j];
+			for (int col=0; col < second.length; col++) {
+				if (first[row] == second[col]) {
+					currentRow[col+1] = prevRow[col];
 				} else {
-					v1[j+1] = Math.min(Math.min(v1[j], v0[j + 1]), v0[j]) + 1;
+					currentRow[col+1] = Math.min(Math.min(currentRow[col], prevRow[col + 1]), prevRow[col]) + 1;
 				}
-				v0[j] = v1[j];
+				prevRow[col] = currentRow[col];
 			}
-			v0[t.length] = v1[t.length];
+			prevRow[second.length] = currentRow[second.length];
 			
 		}
-		return v1[t.length];
+		return currentRow[second.length];
 	}
 	
-	public static boolean smallerOrEqualThan(char[] s, char[] t, int maxDistance) {
+	/**
+	 * Partially computes the Levenshtein distance of
+	 * <code>first</code> and <code>second</code>. Returns true 
+	 * if the computed distance <= <code>maxDistance</code>.
+	 * 
+	 * <p>Running time: O(maxDistance * l) with l = min(|first|, |second|)</p>
+	 */
+	public static boolean smallerOrEqualThan(char[] first, char[] second, int maxDistance) {
 		if (maxDistance < 0) return false;
-		if (maxDistance == 0) return Arrays.equals(s, t);
-		if (Arrays.equals(s, t)) return true;
-		if (s.length == 0) return t.length <= maxDistance;
-		if (t.length == 0) return s.length <= maxDistance;
+		if (maxDistance == 0) return Arrays.equals(first, second);
+		if (Arrays.equals(first, second)) return true;
+		if (first.length == 0) return second.length <= maxDistance;
+		if (second.length == 0) return first.length <= maxDistance;
 		
-		if (t.length > s.length) {
-			char[] temp = t;
-			t = s;
-			s = temp;
+		// Swap the smaller string to save some memory.
+		if (second.length > first.length) {
+			char[] temp = second;
+			second = first;
+			first = temp;
 		}
 		
-		int[] v0 = new int[t.length + 1];
-		int[] v1 = new int[t.length + 1];
+		int[] prevRow = new int[second.length + 1];
+		int[] currentRow = new int[second.length + 1];
 		
-		for (int i=0; i < Math.min(maxDistance + 2, v0.length); i++) {
-			v0[i] = i;
+		// Fill the first row with consecutive replace operation
+		for (int col=0; col < Math.min(maxDistance + 2, prevRow.length); col++) {
+			prevRow[col] = col;
 		}
 		
-		for (int i=0; i < s.length; i++) {
-			v1[0] = i + 1;
-			int lower = Math.max(0, i - maxDistance - 1);
-			int upper = Math.min(t.length, i + maxDistance + 1);
-			for (int j=lower; j < upper; j++) {
-				if (s[i] == t[j]) {
-					v1[j + 1] = v0[j];
+		// Compute the other rows until all letters of first are used.
+		for (int row=0; row < first.length; row++) {
+			currentRow[0] = row + 1;
+			int lower = Math.max(0, row - maxDistance - 1);
+			int upper = Math.min(second.length, row + maxDistance + 1);
+			for (int col=lower; col < upper; col++) {
+				if (first[row] == second[col]) {
+					currentRow[col + 1] = prevRow[col];
 				} else {
-					v1[j + 1] = Math.min(Math.min(v1[j], v0[j + 1]), v0[j]) + 1;
+					currentRow[col + 1] = Math.min(Math.min(currentRow[col], prevRow[col + 1]), prevRow[col]) + 1;
 				}
-				v0[j] = v1[j];
+				prevRow[col] = currentRow[col];
 			}
-			v0[upper] = v1[upper];
+			prevRow[upper] = currentRow[upper];
 		}
-		return v0[t.length] <= maxDistance;
+		return prevRow[second.length] <= maxDistance;
 	}
 }
